d09535fc96dd56b0d26cba9c2f068825cc8df674
etherealfeel <graciagodsyndrome@gmail.com>
2024-02-21 17:37:28 +0200

    feat: add header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+  },
 }
